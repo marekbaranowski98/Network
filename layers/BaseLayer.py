fecbb3530373f444ecd731df3c9e2a56d6382b6e
Marek Baranowski <me@marekbaranowski.net>
2023-08-28 01:13:44 +0200

    feat: add abstract layer

---
+++ layers/BaseLayer.py
@@ -0,0 +1,10 @@
+class BaseLayer:
+    def __init__(self):
+        self.__input = None
+        self.__output = None
+
+    def forward(self, x):
+        raise NotImplementedError
+
+    def backward(self, output_error, learning_rate):
+        raise NotImplementedError
---
+++ layers/__init__.py
@@ -0,0 +1,1 @@
+from .BaseLayer import BaseLayer
