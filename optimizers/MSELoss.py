20213d6a014abf3fea115986501252362e8c0f9c
Marek Baranowski <me@marekbaranowski.net>
2023-08-31 00:30:27 +0200

    fix: calculate mseloss

--- optimizers/MSELoss.py
+++ optimizers/MSELoss.py
@@ -4,8 +4,13 @@ from optimizers import BaseOptimizer
 
 
 class MSELoss(BaseOptimizer):
-    def function_prime(self, y_true: np.array, y_pred: np.array) -> np.array:
-        return np.mean(np.power(y_true, y_pred, 2))
+    def __init__(self, learning_rate):
+        super().__init__(learning_rate)
+
+    def function(self, y_true: np.array, y_pred: np.array) -> np.array:
+        return np.mean(
+            np.power(y_true - y_pred, 2)
+        )
 
     def function_prime(self, y_true: np.array, y_pred: np.array) -> np.array:
         return 2 * (y_pred - y_true) / y_true.size
