7baa98880f11ddfe150cf8b65d6b1ffb27d5ff7a
Marek Baranowski <me@marekbaranowski.net>
2023-08-31 00:27:58 +0200

    feat: add learning rate to optimizer

--- optimizers/BaseOptimizer.py
+++ optimizers/BaseOptimizer.py
@@ -1,4 +1,13 @@
 class BaseOptimizer:
+    def __init__(self, learning_rate):
+        self.__lr = learning_rate
+
+    def update_learning_rate(self, learning_rate):
+        self.__lr = learning_rate
+
+    def get_learning_rate(self):
+        return self.__lr
+
     def function(self, y_true, y_pred):
         raise NotImplementedError
 
